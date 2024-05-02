3a345bcc94ea442b2114d42ff510d26ee07f2571
Mateen54 <mateen.ali@thecherrybyte.com>
2024-05-02 18:02:05 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,94 +1,25 @@
-import * as React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-import RequireAuth from "./routeAuth/RequireAuth";
-import { Spin } from "antd";
-
-const Dashboard = React.lazy(() => import("./components/Admin/Dashboard"));
-const Login = React.lazy(() => import("./components/Login"));
-const BriefReview = React.lazy(() => import("./components/Admin/BriefReview"));
-const Signup = React.lazy(() => import("./components/Signup"));
-const UserManagement = React.lazy(() =>
-  import("./components/Admin/UserManagement")
-);
-const ClientManagement = React.lazy(() =>
-  import("./components/Admin/ClientManagement")
-);
-
-const CenteredLoader = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh", // Adjust this value as needed
-    }}
-  >
-    <Spin size="large" />
-  </div>
-);
-
-const App = () => {
+function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <React.Suspense fallback={<CenteredLoader />}>
-                <Login />
-              </React.Suspense>
-            }
-          />
-
-          <Route
-            path="/Signup"
-            element={
-              <React.Suspense fallback={<CenteredLoader />}>
-                <Signup />
-              </React.Suspense>
-            }
-          />
-
-          <Route
-            path="/dashboard"
-            element={
-              <React.Suspense fallback={<CenteredLoader />}>
-                <Dashboard />
-              </React.Suspense>
-            }
-          />
-          <Route
-            path="/brief-review"
-            element={
-              <React.Suspense fallback={<CenteredLoader />}>
-                <BriefReview />
-              </React.Suspense>
-            }
-          />
-
-          <Route
-            path="/user-management"
-            element={
-              <React.Suspense fallback={<CenteredLoader />}>
-                <UserManagement />
-              </React.Suspense>
-            }
-          />
-
-          <Route
-            path="/client-management"
-            element={
-              <React.Suspense fallback={<CenteredLoader />}>
-                <ClientManagement />
-              </React.Suspense>
-            }
-          />
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
